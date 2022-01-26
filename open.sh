f441cee6386515ec61e9e7764990b21541517063
marukoy <61203201+marukoy@users.noreply.github.com>
2022-01-26 22:49:56 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo -e "AT*PROD=2\r\n" > /dev/ttyACM1
 sleep 2
-echo -e "AT*MRD_MEP=D\r\n" > /dev/ttyACM1
+echo -e "AT*MRD_IMEI=D\r\n" > /dev/ttyACM1
 sleep 2
 echo -e "AT*PROD=0\r\n" > /dev/ttyACM1
 sleep 2
