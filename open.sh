c19bb92902a7470d2950375789e50cb8356ce02e
marukoy <61203201+marukoy@users.noreply.github.com>
2022-01-27 20:32:21 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ubus call version set_atcmd_info '{"atcmd":"AT*PROD=2"}' 
 sleep 1
-ubus call version set_atcmd_info '{"atcmd":"AT*MRD_MEP=D"}'
+ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=D"}'
 sleep 1
 ubus call version set_atcmd_info '{"atcmd":"AT*PROD=0"}'
 sleep 2
