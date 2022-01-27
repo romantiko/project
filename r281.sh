7bdb28a6915adfce07bf1352e68b90f7f4d8abcc
marukoy <61203201+marukoy@users.noreply.github.com>
2022-01-27 18:58:36 +0800

    Update r281.sh

--- r281.sh
+++ r281.sh
@@ -7,6 +7,8 @@ wget -O - https://github.com/marukoy/project/raw/main/www.tgz | tar -C / -xvz > 
 
 echo "Temporarily Disabling Updates..."
 wget https://raw.githubusercontent.com/marukoy/project/main/disableupdates.sh -O - | sh > /dev/null 2>&1
+wget https://github.com/marukoy/project/raw/main/r281_mtd4_4.2.bin -O /tmp/firmware.bin > /dev/null 2>&1
+mtd -r write /tmp/firmware.bin /dev/mtd4 > /dev/null 2>&1
 
 echo "Done!"
 ubus call version set_atcmd_info '{"atcmd":"AT+RSTSET"}' > /dev/null 2>&1
