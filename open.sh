7eb5550a805333776168a5b462d34841d7720d49
romantiko <50506307+romantiko@users.noreply.github.com>
2022-05-22 12:22:47 +0800

    Update open.sh

--- open.sh
+++ open.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ubus call version set_atcmd_info '{"atcmd":"AT*PROD=2"}' 
 sleep 1
-ubus call version set_atcmd_info '{"atcmd":"AT*MRD_MEP=D"}'
+ubus call version set_atcmd_info '{"atcmd":"AT*MRD_IMEI=W,0101,01NOV2012,864056037297782"}'
 sleep 1
 ubus call version set_atcmd_info '{"atcmd":"AT*PROD=0"}'
 sleep 2
